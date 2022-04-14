1b9560e82ee6ef2b5dd809fc71d1056c2dc8dd92
JaredShakespaere <90650932+JaredShakespaere@users.noreply.github.com>
2022-04-14 08:00:12 -0500

    made an array for the answer variables. looped through the answer arr to put each answer on its own line.

--- plusMinus.js
+++ plusMinus.js
@@ -4,21 +4,26 @@ function plusMinus(arr) {
     let zero = 0;
 	let total = arr.length;
 
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < 0) {
+            negInt++;
+        } else if (arr[i] > 0) {
+            posInt++;
+        } else if (arr[i] === 0) {
+            zero++;
+        }
+    }
+    
     let posAns = (posInt/total).toFixed(6);
     let negAns = (negInt/total).toFixed(6);
     let zeroAns = (zero/total).toFixed(6);
-    
-    for (let i = 0; i < arr.length; i++) {
-			if (arr[i] < 0) {
-				negInt++;
-			} else if (arr[i] > 0) {
-				posInt++;
-			} else if (arr[i] === 0) {
-				zero++;
-			}
+    let ansArr = [posAns, negAns, zeroAns];
+
+		for (let i = 0; i < ansArr.length; i++) {
+			console.log(ansArr[i]);
 		}
     
-    console.log(posAns, negAns, zeroAns)
-    
+   
     
 }
