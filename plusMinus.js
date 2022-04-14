af7a37044be105b6ff4156f1691e980c705a63a7
JaredShakespaere <90650932+JaredShakespaere@users.noreply.github.com>
2022-04-14 07:49:53 -0500

    created an zero variable and a for loop with logic

--- plusMinus.js
+++ plusMinus.js
@@ -1,6 +1,16 @@
 function plusMinus(arr) {
     let posInt = 0;
-		let negInt = 0;
-		let total = arr.length;
+	let negInt = 0;
+    let zero = 0;
+	let total = arr.length;
     
+    for (let i = 0; i < arr.length; i++) {
+			if (arr[i] < 0) {
+				negInt++;
+			} else if (arr[i] > 0) {
+				posInt++;
+			} else if (arr[i] === 0) {
+				zero++;
+			}
+		}
 }
